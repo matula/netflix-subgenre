98ab8b630bfe012a12ee27cd84b6a5aaacb99521
Matula <terrymatula@gmail.com>
2013-11-16 12:01:09 -0600

    First commit

---
+++ app/views/home.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Netflix Sub-genre Generator">
+    <meta name="author" content="Terry Matula">
+
+    <title>Netflix Sub-genre Generator</title>
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Netflix<sup>&reg;</sup> Sub-genre Generator</a>
+        </div>
+        <div class="pull-right disclaimer">Not affiliated with Netflix<sup>&reg;</sup> in any way. This is just for fun.</div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="headline">
+        <p class="lead">
+          When viewing the list of movies in sub-genres, <a href="http://netflix.com" target="_blank">Netflix</a> sometimes comes up with quite interesting ones.<br>
+          Here's a new suggestion!
+        </p>
+      </div>
+      <div class="genre-box">
+
+      </div>
+
+    </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+  </body>
+</html>
